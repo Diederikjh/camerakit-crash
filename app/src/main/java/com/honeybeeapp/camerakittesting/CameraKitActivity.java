5c206b66d06f7bd2f7efd5b7658b59d17250e135
Diederik Hattingh <djh@emss.co.za>
2018-05-18 14:16:53 +0200

    Camera kit 1.0.0 testing

--- app/src/main/java/com/honeybeeapp/camerakittesting/CameraKitActivity.java
+++ app/src/main/java/com/honeybeeapp/camerakittesting/CameraKitActivity.java
@@ -1,32 +1,37 @@
 package com.honeybeeapp.camerakittesting;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 
-import com.wonderkiln.camerakit.CameraView;
+import com.camerakit.CameraKitView;
 
 public class CameraKitActivity extends AppCompatActivity {
 
-    private CameraView mCameraView;
+    private CameraKitView mCameraView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera_kit);
-
         mCameraView = findViewById(R.id.camera);
-
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        mCameraView.start();
+        mCameraView.onResume();
     }
 
     @Override
     protected void onPause() {
+        mCameraView.onPause();
         super.onPause();
-        mCameraView.stop();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        mCameraView.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
